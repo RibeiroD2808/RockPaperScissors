6f78531878e8f139fa421840769f9980991e4f71
RibeiroD2808 <diogo.ribeiro.2808@gmail.com>
2023-09-06 00:33:56 +0100

    align all the content, and update buttons image

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const rockButton = document.querySelector('#rockButton');
+const paperButton = document.querySelector('#paperButton');
+const scissorButton = document.querySelector('#scissorButton');
+
+const resultP = document.querySelector('#result');
+const playerScoreP = document.querySelector('#playerScore');
+const computerScoreP = document.querySelector('#computerScore');
 
 const scissor = "scissor";
 const rock = "rock";
@@ -5,6 +12,27 @@ const paper = "paper";
 
 var playerScore = 0;
 var computerScore = 0;
+
+paperButton.addEventListener('click', () => {
+    //alert("Hello World");
+    roud(paper);
+});
+
+rockButton.addEventListener('click', () => {
+    //alert("Hello World");
+    roud(rock);
+});
+
+scissorButton.addEventListener('click', () => {
+    //alert("Hello World");
+    roud(scissor);
+});
+
+function updateScore()
+{
+    computerScoreP.textContent = computerScore;
+    playerScoreP.textContent = playerScore;
+}
 
 function getComputerChoice(){
     let a = Math.floor(Math.random() * 3)
@@ -17,47 +45,56 @@ function getComputerChoice(){
         return scissor
 }
 
-function roud(){
+function roud(playerChoice){
 
+    
+    
     const computerChoice  = getComputerChoice();
 
-    let playerChoice = prompt();
+    //let playerChoice = prompt();
 
     playerChoice = playerChoice.toLowerCase();
     
     console.log("Player: " + playerChoice);
     console.log("Computer: " + computerChoice);
 
-    if(computerChoice == playerChoice)
-        console.log("its a draw");
+    if(computerChoice == playerChoice){
+        console.log("Its a draw");
+        resultP.textContent = "Its a draw";
+    }
     else if(playerChoice == scissor && computerChoice == paper){
         console.log("You Won! \nCPU played " + computerChoice);
+        resultP.textContent = "You Won! \nCPU played " + computerChoice;
         playerScore ++;
     }
     else if(playerChoice == rock && computerChoice == scissor){
         console.log("You Won! \nCPU played " + computerChoice);
+        resultP.textContent = "You Won! \nCPU played " + computerChoice;
         playerScore ++;
     }   
     else if(playerChoice == paper && computerChoice == rock){
         console.log("You Won! \nCPU played " + computerChoice);
+        resultP.textContent = "You Won! \nCPU played " + computerChoice;
         playerScore ++;
     }   
     else
     {
         console.log("You Lost! \nCPU played " + computerChoice);
+        resultP.textContent = "You Lost! \nCPU played " + computerChoice
         computerScore ++;
     }
 
-}
-
-function game(){
+    updateScore();   
     
-    for (let i = 1; i <= 5; i++)
+    if(playerScore >= 5 || computerScore >= 5)
     {
-        console.log("-------------- Round " + i + " --------------");
-        roud();
-        console.log("Player: " + playerScore + "\nComputer: " + computerScore);
+        
+        document.body.innerHTML = ' ';
+        const finalText = document.createElement('p');
+        if(playerScore >= 5)
+            finalText.textContent = "YOU WON";
+        if(computerScore >= 5)
+            finalText.textContent = "YOU LOST";
+        document.body.appendChild(finalText);
     }
 }
-
-game();
